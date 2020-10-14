122401a9e15d4f8bcd36ba16eefcce215677e6b4
starxmaker <beneath.black.skies@gmail.com>
2020-10-14 01:40:08 -0300

    Updates

---
+++ precache-manifest.0c144c95623ea6d9bb3c3d88df47c93a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc71b2b1ad5a7c8efe25f38d9fe6798",
+    "url": "/starxmaker/rcch.git/index.html"
+  },
+  {
+    "revision": "c227d6d9a3033b06ddad",
+    "url": "/starxmaker/rcch.git/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fce38e41905eaee04172",
+    "url": "/starxmaker/rcch.git/static/css/main.be1f754b.chunk.css"
+  },
+  {
+    "revision": "c227d6d9a3033b06ddad",
+    "url": "/starxmaker/rcch.git/static/js/2.98e7e03f.chunk.js"
+  },
+  {
+    "revision": "2a5ef035c91d80285eda69516dffe195",
+    "url": "/starxmaker/rcch.git/static/js/2.98e7e03f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce38e41905eaee04172",
+    "url": "/starxmaker/rcch.git/static/js/main.0e32a083.chunk.js"
+  },
+  {
+    "revision": "13fc723124762fa0a94b",
+    "url": "/starxmaker/rcch.git/static/js/runtime-main.e0ac6806.js"
+  }
+]);
